b80a381ba26b47cfd769f63ea784d4bf86655036
karen <kayy.thoms@gmail.com>
2024-09-16 15:56:11 -0400

    mini start up

---
+++ Timer Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Acceleration")]
+    public float accel = 1f;
+    public float horiAccel = 1f;
+    public float vertAccel = .1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+}
